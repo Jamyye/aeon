dbdedf67c47a0591056d73dab1156db0f9da0de7
Jamyye <33551349+Jamyye@users.noreply.github.com>
2018-08-28 15:44:16 -0500

    Update difficulty.h

--- src/cryptonote_basic/difficulty.h
+++ src/cryptonote_basic/difficulty.h
@@ -56,4 +56,3 @@ namespace cryptonote
     difficulty_type next_difficulty_v2(std::vector<std::uint64_t> timestamps, std::vector<difficulty_type> cumulative_difficulties, size_t target_seconds);
 }
 
-}
